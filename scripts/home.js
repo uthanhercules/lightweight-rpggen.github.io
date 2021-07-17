c399f90e4fe1d939c30ddbc253538faaff43dc62
Uthan H <uthan.hercules12@gmail.com>
2021-07-17 04:55:39 -0300

    Utilities page done

--- scripts/home.js
+++ scripts/home.js
@@ -1,5 +1,5 @@
 function redirect(url) {
-    if (url === './pages/about.html') {
+    if (url === './pages/about.html' || url === './pages/utilities.html') {
         window.location.href = url;
     } else {
         alert('Under construction');
