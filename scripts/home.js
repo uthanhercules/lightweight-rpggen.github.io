7941ffbee0b5a7c52cae7cc6ab095d413f30d537
Uthan H <uthan.hercules12@gmail.com>
2021-07-17 07:11:36 -0300

    Buttons now works properly

--- scripts/home.js
+++ scripts/home.js
@@ -1,5 +1,5 @@
 function redirect(url) {
-    const list_of_url = ['./pages/about.html', './pages/utilities.html'];
+    const list_of_url = ['./pages/about.html', './pages/utilities.html', './name_generator.html'];
     if (list_of_url.includes(url)) {
         window.location.href = url;
     } else {
